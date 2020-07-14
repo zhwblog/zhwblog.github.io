2e774396fcae1936d673c427aa20a1b818856512
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-07-14 14:20:30 +0800

    Update showFooter.js

--- script/showFooter.js
+++ script/showFooter.js
@@ -1,4 +1,4 @@
 var version = "1.0.1"
 var years = "2020"
 var date = "Jul 14th,2020"
-document.write('<p style="font-family:Consolas;color:white;">Kevinblog ' + version + ' | Designed by (©) ' + years + ' OIstream & ASFOS | 更新于 ' + date + ' | <a href="malito:kevinzhw153@126.com" style="color:white;">联系我们</a>（kevinzhw153@126.com）' + ' | <a href="https://kevinzhw153.github.io/new.txt" style="color:white;">更新记录</a></p>')
+document.write('<p style="font-family:Consolas;color:white;">Kevinblog ' + version + ' | Designed by (©) ' + years + ' kevin | 更新于 ' + date + ' | <a href="malito:kevinzhw153@126.com" style="color:white;">联系我们</a>（kevinzhw153@126.com）' + ' | <a href="https://kevinzhw153.github.io/new.txt" style="color:white;">更新记录</a></p>')
