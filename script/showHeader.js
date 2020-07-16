10356c252957837425c62ce6805b05705c2df936
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-07-16 09:57:41 +0800

    Update showHeader.js

--- script/showHeader.js
+++ script/showHeader.js
@@ -1,3 +1,3 @@
-var sChn = "hhh"
+var sChn = "呃呃呃"
 var sEng = "Em!"
 document.write('<div class="sentence-chn"><strong>' + sChn + '</strong></div><div class="line"><div class="S">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div><div>·</div><div class="S">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div></div><div class="sentence-eng"><strong>' + sEng + '</strong></div>')
