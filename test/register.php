5e85a06ffeee26e1ed4a201675951c31fba393ac
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-08-08 21:34:59 +0800

    Update register.php

--- test/register.php
+++ test/register.php
@@ -1,56 +1,91 @@
-<?php
-    include_once("function/fileSystem.php");
-    include_once("function/database.php");
-
-    if (empty($_POST)) {
-        exit("您提交的表单数据超过post_max_size! <br>");
-    }
-
-    // 判断输入密码与确认密码是否相同
-    $password = $_POST['password'];
-    $confirmPassword = $_POST['confirmPassword'];
-    if ($password != $confirmPassword) {
-        exit("输入的密码与确认密码不相等！");
-    }
-
-    $userName = $_POST['userName'];
-    $domain = $_POST['domain'];
-    $userName = $userName . $domain;
-
-    // 判断用户名是否重复
-    $userNameSQL = "select * from users where userName = '$userName'";
-    getConnect();
-    $resultSet = mysql_query($userNameSQL);
-    if (mysql_num_rows($resultSet) > 0) {
-        exit("用户名已被占用，请更换其他用户名");
-    }
-
-    $sex = $_POST['sex'];
-    if (empty($_POST['interests'])) {
-        $interests = "";
-    } else {
-        $interests = implode(";", $_POST['interests']);
-    }
-
-    $remark = $_POST['remark'];
-    $myPictureName = $_FILES['myPicture']['name'];
-
-    $registerSQL = "insert into users values(null, '$userName', '$password', '$sex', '$interests', '$myPictureName', '$remark')";
-    $message = upload($_FILES['myPicture'], "uploads");
-
-    if ($message == "上传成功" || $message == "没有上传") {
-        mysql_query($registerSQL);
-        $userID = mysql_insert_id();
-        echo "注册成功<br>";
-    } else {
-        exit($message);
-    }
-
-    $userSQL = "select * from users where user_id = '$userID'";
-    $userResult = mysql_query($userSQL);
-    if ($user = mysql_fetch_array($userResult)) {
-        echo "您的注册用户名为：" . $user['userName'];
-    } else {
-        exit("用户注册失败！");
-    }
-    closeConnect();
+<!DOCTYPE html>
+<html>
+<head>
+<title>注册</title>
+<meta name="content-type"; charset=UTF-8">
+</head>
+<body>
+    <div class="content" align="center">
+        <!--头部-->
+        <div class="header">
+        <h1>注册页面</h1>
+        </div>
+        <!--中部-->
+        <div class="middle">
+            <form action="registeraction.php" method="post">
+                <table border="0">
+                    <tr>
+                        <td>用户名：</td>
+                        <td><input type="text" id="id_name" name="username" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td>密&nbsp;&nbsp;&nbsp;码：</td>
+                        <td><input type="password" id="password" name="password" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td>重复密码：</td>
+                        <td><input type="password" id="re_password" name="re_password" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td>性别：</td>
+                        <td>
+                            <input type="radio" id="sex" name="sex" value="男">男
+                            <input type="radio" id="sex" name="sex" value="女">女
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>QQ：</td>
+                        <td><input type="text" id="qq" name="qq" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td>Email：</td>
+                        <td><input type="email" id="email" name="email" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td>电话：</td>
+                        <td><input type="text" id="phone" name="phone" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td>地址：</td>
+                        <td><input type="text" id="address" name="address" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" align="center" style="color:red;font-size:10px;">
+                        <!--提示信息-->
+                            <?php
+                                $err=isset($_GET["err"])?$_GET["err"]:"";
+                                switch($err) {
+                                    case 1:
+                                    echo "用户名已存在！";
+                                    break;
+                                    case 2:
+                                    echo "密码与重复密码不一致！";
+                                    break;
+                                    case 3:
+                                    echo "注册成功！";
+                                    break;
+                                }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" align="center">
+                            <input type="submit" id="register" name="register" value="注册">
+                            <input type="reset" id="reset" name="reset" value="重置">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" align="center">
+                            如果已有账号，快去<a href="login.php">登录</a>吧！
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        <!--脚部-->
+        <div class="footer">
+        <small>Copyright &copy; 版权所有·欢迎翻版
+        </div>
+    </div>
+</body>
+</html>
