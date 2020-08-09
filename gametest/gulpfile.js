335695aa4b9b9497620db3adb8b13eddb5bd29c7
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-08-09 09:46:42 +0800

    Update gulpfile.js

--- gametest/gulpfile.js
+++ gametest/gulpfile.js
@@ -1,13 +1,3 @@
-/**
- * @file Gulp File
- * @author BenzLeung(https://github.com/BenzLeung)
- * @date 2017/2/4
- * Created by JetBrains PhpStorm.
- *
- * 每位工程师都有保持代码优雅的义务
- * each engineer has a duty to keep the code elegant
- */
-
 var gulp = require('gulp');
 var bump = require('gulp-bump');
 var clean = require('gulp-clean');
