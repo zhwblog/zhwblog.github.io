215f2701aa595428d27f100bd28383010f22766e
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-08-16 11:08:10 +0800

    Update languageScene.js

--- gametest/js/scenes/languageScene.js
+++ gametest/js/scenes/languageScene.js
@@ -1,13 +1,3 @@
-/**
- * @file 语言选择
- * @author BenzLeung(https://github.com/BenzLeung)
- * @date 2017/2/7
- * Created by JetBrains PhpStorm.
- *
- * 每位工程师都有保持代码优雅的义务
- * each engineer has a duty to keep the code elegant
- */
-
 define(
     [
         'cocos',
