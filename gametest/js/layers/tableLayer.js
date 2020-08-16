9ab868ab46e30559209bdbc0821ff9761aff9b48
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-08-16 11:19:12 +0800

    Update tableLayer.js

--- gametest/js/layers/tableLayer.js
+++ gametest/js/layers/tableLayer.js
@@ -1,14 +1,3 @@
-/**
- * @file 台球桌
- * @author BenzLeung(https://github.com/BenzLeung)
- * @date 2017/2/20
- * @class TableLayer
- * Created by JetBrains PhpStorm.
- *
- * 每位工程师都有保持代码优雅的义务
- * each engineer has a duty to keep the code elegant
- */
-
 define([
     'cocos',
     'chipmunk',
