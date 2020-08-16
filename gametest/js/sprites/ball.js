d063738383d5943ccf853200a81a3e27f084a041
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-08-16 11:12:13 +0800

    Update ball.js

--- gametest/js/sprites/ball.js
+++ gametest/js/sprites/ball.js
@@ -1,14 +1,3 @@
-/**
- * @file 球
- * @author BenzLeung(https://github.com/BenzLeung)
- * @date 2017/2/18
- * @class Ball
- * Created by JetBrains PhpStorm.
- *
- * 每位工程师都有保持代码优雅的义务
- * each engineer has a duty to keep the code elegant
- */
-
 define(['cocos', 'chipmunk'], function (cc, cp) {
 
     return cc.PhysicsSprite.extend({
