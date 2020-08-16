a97499f98b4286208124677458a4c3091b1eeb1f
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-08-16 11:11:24 +0800

    Update requireMain.js

--- gametest/js/requireMain.js
+++ gametest/js/requireMain.js
@@ -1,13 +1,3 @@
-/**
- * @file RequireJS 的初始化配置
- * @author BenzLeung(https://github.com/BenzLeung)
- * @date 2017/2/3
- * Created by JetBrains PhpStorm.
- *
- * 每位工程师都有保持代码优雅的义务
- * each engineer has a duty to keep the code elegant
- */
-
 require.config({
     baseUrl: 'js',
     paths: {
