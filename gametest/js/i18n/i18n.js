ebbc6fdf2bc19b9865d63c8626ac367686219454
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-08-16 11:17:23 +0800

    Update i18n.js

--- gametest/js/i18n/i18n.js
+++ gametest/js/i18n/i18n.js
@@ -1,14 +1,3 @@
-/**
- * @file 自制的多语言文本转换库
- * @author BenzLeung(https://github.com/BenzLeung)
- * @date 2017/2/5
- * @module i18n
- * Created by JetBrains PhpStorm.
- *
- * 每位工程师都有保持代码优雅的义务
- * each engineer has a duty to keep the code elegant
- */
-
 define(['cocos', 'i18n/localeList'], function (cc, localeList) {
     var localeContent = {};
 
