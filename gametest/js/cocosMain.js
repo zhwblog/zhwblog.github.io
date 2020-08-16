ceb27dd7d2472c52fa00b71cf6ad786ba700c959
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-08-16 11:16:30 +0800

    Update cocosMain.js

--- gametest/js/cocosMain.js
+++ gametest/js/cocosMain.js
@@ -1,13 +1,3 @@
-/**
- * @file 用于学习cocos
- * @author BenzLeung(https://github.com/BenzLeung)
- * @date 2016/12/20
- * Created by JetBrains PhpStorm.
- *
- * 每位工程师都有保持代码优雅的义务
- * each engineer has a duty to keep the code elegant
- */
-
 require(['cocos', 'scenes/titleScene', 'benzAudioEngine'], function (cc, TitleScene, benzAudioEngine) {
 
     window.DEBUG_MODE = 1;
