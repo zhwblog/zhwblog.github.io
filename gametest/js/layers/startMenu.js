c9d7fb458189fee533ce3108e6f3f09ddaeb519a
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-08-16 11:18:48 +0800

    Update startMenu.js

--- gametest/js/layers/startMenu.js
+++ gametest/js/layers/startMenu.js
@@ -1,14 +1,3 @@
-/**
- * @file 游戏开始菜单
- * @author BenzLeung(https://github.com/BenzLeung)
- * @date 2017/3/5
- * @class StartMenu
- * Created by JetBrains PhpStorm.
- *
- * 每位工程师都有保持代码优雅的义务
- * each engineer has a duty to keep the code elegant
- */
-
 define(
     [
         'cocos',
