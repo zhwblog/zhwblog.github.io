b3f32b5d620a65bef98d735af35c1fa5f07c25d4
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-08-16 11:13:09 +0800

    Update ballAnimate.js

--- gametest/js/sprites/ballAnimate.js
+++ gametest/js/sprites/ballAnimate.js
@@ -1,14 +1,3 @@
-/**
- * @file 带动画的球
- * @author BenzLeung(https://github.com/BenzLeung)
- * @date 2017/3/3
- * @class BallAnimate
- * Created by JetBrains PhpStorm.
- *
- * 每位工程师都有保持代码优雅的义务
- * each engineer has a duty to keep the code elegant
- */
-
 define(['cocos', 'sprites/ball'], function (cc, Ball) {
     var BALL_FRAME_NUM = 7;
 
