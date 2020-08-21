e605bcd215d0a5486887e0274074bff63dd6525a
kevinzhw153 <56783288+kevinzhw153@users.noreply.github.com>
2020-08-21 15:43:09 +0800

    Update showTool.js

--- script/showTool.js
+++ script/showTool.js
@@ -1,4 +1,4 @@
-document.write('<div id="toolBox"><div id="toolBoxUp"><div style="float:right;"><button onclick="hideToolBox()"><i class="fa fa-close"></i></button></div></div><div id="toolBoxMain"><button class="newButton" title="喜欢这张背景图？点击全屏显示" onclick="showBgImg()" id="needChange"><i class="fa fa-expand"></i></button></div></div><div id="toolBoxHide" style="display:none;"><div style="text-align:center;"><button onclick="showToolBox()"><i class="fa fa-angle-up"></i></button></div></div>')
+document.write('<div id="toolBox"><div id="toolBoxUp"><div style="float:right;"><button onclick="hideToolBox()"><i class="fa fa-close"></i></button></div></div><div id="toolBoxMain"><button class="newButton" title="不喜欢这张背景图？点击换一张" onclick="refresh()"><i class="fa fa-refresh"></i></button><button class="newButton" title="喜欢这张背景图？点击全屏显示" onclick="showBgImg()" id="needChange"><i class="fa fa-expand"></i></button></div></div><div id="toolBoxHide" style="display:none;"><div style="text-align:center;"><button onclick="showToolBox()"><i class="fa fa-angle-up"></i></button></div></div>')
 /*
 <div id="toolBox">
 		<div id="toolBoxUp">
@@ -7,7 +7,7 @@ document.write('<div id="toolBox"><div id="toolBoxUp"><div style="float:right;">
 			</div>
 		</div>
 		<div id="toolBoxMain">
-	<button class="newButton" title="喜欢这张背景图？点击全屏显示" onclick="showBgImg()" id="needChange"><i class="fa fa-expand"></i></button>
+			<button class="newButton" title="不喜欢这张背景图？点击换一张" onclick="refresh()"><i class="fa fa-refresh"></i></button><button class="newButton" title="喜欢这张背景图？点击全屏显示" onclick="showBgImg()" id="needChange"><i class="fa fa-expand"></i></button>
 		</div>
 	</div>
 	<div id="toolBoxHide" style="display:none;">
